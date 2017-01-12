96f4413162613abaf976d3224d837b4ed2822e75
sarahyoon <yoonsarah88@gmail.com>
2017-01-12 23:40:31 +0900

    Add Audio Extension

--- PitchPerfect_sy/PlaySoundViewController.swift
+++ PitchPerfect_sy/PlaySoundViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundViewController: UIViewController {
     
@@ -19,6 +20,13 @@ class PlaySoundViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     
     var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb}
+    
     
     @IBAction func playSoundForButton(_ sender: UIButton){
         print("play sound button pressed")
@@ -32,16 +40,17 @@ class PlaySoundViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupAudio()
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
-
+    
     /*
     // MARK: - Navigation
 
